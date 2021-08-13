0e13c9744baccdb7ac803a88a2a5e09fc4dcee99
Anupam Dey <anupam970.dey@gmail.com>
2021-08-13 20:23:20 +0530

    Initial project commit

--- exploration_of_singlesentence_input.py
+++ exploration_of_singlesentence_input.py
@@ -1,18 +1,21 @@
 import joblib
-joblib_file = "joblib_MultinomialNAiveBayes_Model.pkl"  
-joblib_MNB_model = joblib.load(joblib_file)
-
-##########################################################################################################
 import numpy as np
 import pandas as pd
-
 import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+
+joblib_file = "joblib_MultinomialNAiveBayes_Model.pkl"  
+joblib_MNB_model = joblib.load(joblib_file)
+
+##########################################################################################################
+
 inputStmt = "It is a friggging awesome place !!!!"
 corpus = []
 
+#cleaning input-data
 review = re.sub('[^a-zA-Z]', ' ', inputStmt)
 review = review.lower()
 review = review.split()
@@ -21,24 +24,13 @@ review = [ps.stem(word) for word in review if not word in set(stopwords.words('e
 review = ' '.join(review)
 corpus.append(review)
 
-    
-    
-# Creating the Bag of Words model using CountVectorizer
-
-from sklearn.feature_extraction.text import CountVectorizer
-
+#import vectorizer
 vectorizer_file_name = "vectorizer.pkl"
 loaded_vectorizer = joblib.load(vectorizer_file_name)
 
 #X = loaded_vectorizer.transform(corpus).toarray()
 #X = loaded_vectorizer.transform(corpus).toarray()
 
-
-#########################################################################################################
-
-
-#y_pred = joblib_MNB_model.predict(X)
-#print(y_pred)
 result = (joblib_MNB_model.predict(loaded_vectorizer.transform(corpus)))
 
 if result == 0:
